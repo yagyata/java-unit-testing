9aa2637314d132d39ce80ac5522d35ac1bd21d20
Yagyata <yagyatasharmaa@gmail.com>
2025-04-02 20:32:38 +0530

    [Yagyata] ADD: Java Unit Testing (JUnit)

---
+++ src/main/java/com/bridgelabz/junit/Calculator.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.junit;
+
+public class Calculator {
+
+    public static int add(int a, int b) {
+        return a+b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a-b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a*b;
+    }
+
+    public static double divide(int a, int b) {
+        if(b==0) {
+            throw new ArithmeticException("Can't be divided by zero");
+        }
+        return a/b;
+    }
+}
---
+++ src/test/java/testing/CalculatorTesting.java
@@ -0,0 +1,48 @@
+package testing;
+
+import com.bridgelabz.junit.Calculator;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTesting {
+
+    //test method for add
+    @Test
+    public void addTest() {
+        int result = Calculator.add(15, 32);
+        int expected = 47;
+        assertEquals(expected,result);
+        assertEquals(22,Calculator.add(17,5));
+    }
+
+    //test method for subtract
+    @Test
+    public void subtractTest() {
+        int result = Calculator.subtract(24,7);
+        int expected = 17;
+        assertEquals(expected, result);
+    }
+
+    //test method for multiply
+    @Test
+    public void multiplyTest() {
+        int result = Calculator.multiply(7, 8);
+        int expected = 56;
+        assertEquals(expected, result);
+    }
+
+    //test method for divide
+    @Test
+    public void divideTest() {
+        double result = Calculator.divide(21, 3);
+        double expected = 7;
+        assertEquals(expected, result, 0.0001);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void divideByZeroTest() {
+        Calculator.divide(1, 0);
+    }
+
+}
