94aa1c856e515ff18d473cbaea46c44fb44d23c6
Yagyata <yagyatasharmaa@gmail.com>
2025-04-03 22:12:20 +0530

    [Yagyata] ADD: Testing Temperature Converter

---
+++ src/main/java/com/bridgelabz/junit/TemperatureConverter.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.junit;
+
+public class TemperatureConverter {
+    public static double celsiusToFahrenheit(double celsius) {
+        double fahrenheit = (celsius * 9/5) + 32;
+        return fahrenheit;
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        double celsius = (fahrenheit - 32 ) * 5/9;
+        return celsius;
+    }
+}
---
+++ src/test/java/com/bridgelabz/testing/TemperatureConverterTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.testing;
+
+import com.bridgelabz.junit.TemperatureConverter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TemperatureConverterTest {
+
+    @Test
+    void celsiusToFahrenheitTest() {
+        assertEquals(98.6, TemperatureConverter.celsiusToFahrenheit(37), 0.01);
+    }
+
+    @Test
+    void fahrenheitToCelsiusTest() {
+        assertEquals(37, TemperatureConverter.fahrenheitToCelsius(98.6), 0.01);
+    }
+
+}
