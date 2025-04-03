28c63f4f1161f7b83e44f1caf5366d1a8aa5bc40
Yagyata <yagyatasharmaa@gmail.com>
2025-04-03 15:42:30 +0530

    [Yagyata] ADD: Testing File Handling Methods

---
+++ src/main/java/com/bridgelabz/junit/FileProcessor.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.junit;
+
+import java.io.*;
+
+public class FileProcessor {
+    public static void writeToFile(String filename, String content) throws IOException {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            writer.write(content);
+        }
+    }
+
+    public static String readFromFile(String filename) throws IOException{
+        StringBuilder content = new StringBuilder();
+        try(BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        }
+        return content.toString().trim();
+    }
+}
---
+++ src/test/java/com/bridgelabz/testing/FileProcessorTest.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.testing;
+
+import com.bridgelabz.junit.FileProcessor;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FileProcessorTest{
+    static final String fileName = "file.txt";
+    String content = "Hello! My name is Yagyata.";
+
+    @Test
+    public void shouldWriteAndWriteToFile() throws IOException {
+        FileProcessor.writeToFile(fileName,content);
+        String readContent = FileProcessor.readFromFile(fileName);
+        assertEquals(readContent, content);
+    }
+
+    @Test
+    public void testFileExistenceAfterWriting() {
+        File file = new File(fileName);
+        assertTrue(file.exists());
+    }
+
+    @Test
+    public void testIOExceptionIfFileAbsent() {
+        assertThrows(IOException.class, () -> FileProcessor.readFromFile("non-existing-file.txt"));
+    }
+
+}
